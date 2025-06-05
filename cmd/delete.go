2cc3ac0a7944a86753c6e8d124b223f8823a68d9
Miguel Liezun <muragnarokstaff@gmail.com>
2025-06-05 11:30:13 +0000

    list bucket

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,10 +1,15 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 var deleteCmd = &cobra.Command{
@@ -17,25 +22,46 @@ Example:
 s3magic delete /path/to/objects_to_delete.txt`,
 	Args: cobra.ExactArgs(1), // Expect exactly one argument: the file path
 	Run: func(cmd *cobra.Command, args []string) {
-		filePath := args[0]
-		fmt.Printf("Attempting to read list of objects to delete from file: %s\n", filePath)
-
-		// In a real implementation, you would add logic here to:
-		// 1. Read and parse the specified file.
-		// 2. For each object in the file, interact with the AWS SDK to perform the deletion.
-		// 3. Handle errors (e.g., file not found, malformed file, S3 deletion errors).
-		// 4. Provide progress and summary feedback.
-
-		// Dummy implementation:
-		// Check if file exists as a basic validation
-		if _, err := os.Stat(filePath); os.IsNotExist(err) {
-			fmt.Fprintf(os.Stderr, "Error: File not found at %s\n", filePath)
+		// Using the SDK's default configuration, load additional config
+		// and credentials values from the environment variables, shared
+		// credentials, and shared configuration files
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
 			os.Exit(1) // Exit with error status
 			return
 		}
+		// Create an Amazon S3 service client
+		client := s3.NewFromConfig(cfg)
 
-		fmt.Printf("Simulating batch deletion of objects listed in: %s\n", filePath)
-		fmt.Println("This is a dummy implementation. No actual deletion will occur.")
+		// Get the first page of results for ListObjectsV2 for a bucket
+		output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+			Bucket: aws.String(args[0]),
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("first page results")
+		for _, object := range output.Contents {
+			fmt.Printf("key=%s size=%d", aws.ToString(object.Key), *object.Size)
+		}
+
+		// // In a real implementation, you would add logic here to:
+		// // 1. Read and parse the specified file.
+		// // 2. For each object in the file, interact with the AWS SDK to perform the deletion.
+		// // 3. Handle errors (e.g., file not found, malformed file, S3 deletion errors).
+		// // 4. Provide progress and summary feedback.
+
+		// // Dummy implementation:
+		// // Check if file exists as a basic validation
+		// if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		// 	fmt.Fprintf(os.Stderr, "Error: File not found at %s\n", filePath)
+		// 	os.Exit(1) // Exit with error status
+		// 	return
+		// }
+
+		// fmt.Printf("Simulating batch deletion of objects listed in: %s\n", filePath)
+		// fmt.Println("This is a dummy implementation. No actual deletion will occur.")
 	},
 }
 
